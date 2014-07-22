9bd3d5ec001a2148190180abf860c7e4dc72810e
yervandakopyan <yervandakopyan@gmail.com>
2014-07-22 00:19:50 -0700

    Create cors.php

---
+++ cors.php
@@ -0,0 +1,23 @@
+<?php
+
+class cors {
+
+    public function __construct()
+    {
+        $allowed_domains = array(
+            'http://localhost',
+            'http://localhost:8000',
+            '*.*.sbx.4over.com'
+        );
+        
+        if(in_array($_SERVER['HTTP_ORIGIN'], $allowed_domains)){
+            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])){
+                header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+            }
+            header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+            header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, OPTIONS, DELETE");
+        }
+        
+    }
+
+}
